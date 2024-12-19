308e98bd2eb1792e85426d3226d258ca348ed021
Elad Karni <elad.karni@gmail.com>
2024-12-19 07:23:59 -0500

    Fix: missed baseURL & preview image

--- src/util/metadataConstructor.ts
+++ src/util/metadataConstructor.ts
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 export function metadataConstructor({
   title = "Micro Precision Machining ",
   description = "Text needed for description of website. This is what is going to show up when you share the website on social media or it is a result on search engines like Google.",
-  image = "url-preview-image.png",
+  image = "url-preview-image.jpg",
   icons = "/favicon.ico",
   noIndex = false,
   customMetadata = {},
@@ -27,17 +27,8 @@ export function metadataConstructor({
         },
       ],
     },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      images: [image],
-      creator: "@crux_psychotherapy",
-    },
     icons,
-    metadataBase: new URL(
-      "https://deploy-preview-11--thecruxpsychotherapy.netlify.app/"
-    ),
+    metadataBase: new URL("https://micro-precision-machining.netlify.app/"),
     ...(noIndex && {
       robots: {
         index: false,
