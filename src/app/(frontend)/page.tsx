5d56f10392959a92501f2ae74110d173ea64b068
Elad Karni <elad.karni@gmail.com>
2024-12-21 17:47:34 -0500

    Fix (Final?): Reset DB

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -7,12 +7,11 @@ import ContactSection from "@/views/contact";
 export default async function Home() {
   return (
     <main className="flex flex-col gap-y-16 justify-center max-w-3xl m-4 md:mx-auto">
-      {/* <HeroSection />
-      <AboutSection />
+      <HeroSection />
+      {/* <AboutSection />
       <FounderSection />
       <FeaturesSection />
       <ContactSection /> */}
-      <h1>Home Page</h1>
     </main>
   );
 }
